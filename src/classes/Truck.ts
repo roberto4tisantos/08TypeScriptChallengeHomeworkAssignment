2dfccbad57981fbd85ff4607cf1adeb155133e8c
FORTIS Roberto <roberto.4ti@gmail.com>
2024-11-04 17:50:15 -0500

    Truck.ts modified

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -8,6 +8,8 @@ import AbleToTow from '../interfaces/AbleToTow.js';
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
 class Truck extends Vehicle {
   // TODO: Declare properties of the Truck class
+  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
+  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
       vin: string;
       color: string;
       make: string;
@@ -17,26 +19,23 @@ class Truck extends Vehicle {
       topSpeed: number;
       wheels: Wheel[];
 
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
-
   // TODO: Create a constructor that accepts the properties of the Truck class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Truck class
-    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
-    constructor(
-      vin: string,
-      color: string,
-      make: string,
-      model: string,
-      year: number,
-      weight: number,
-      topSpeed: number,
-      wheels: Wheel[]
-    ) {
+  // TODO: The constructor should call the constructor of the parent class, Vehicle
+  // TODO: The constructor should initialize the properties of the Truck class
+  // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[]    
+  ) {
       // Call the constructor of the parent class, Vehicle
       super();
-      // Initialize properties of the Car class
+
       this.vin = vin;
       this.color = color;
       this.make = make;
@@ -44,14 +43,17 @@ class Truck extends Vehicle {
       this.year = year;
       this.weight = weight;
       this.topSpeed = topSpeed;
+      this.wheels = wheels;
+
       // Check if the wheels array has 4 elements
       // If not, create 4 new Wheel objects
       // Otherwise, use the provided wheels array
-      if (wheels.length !== 4) {
+      if (this.wheels.length !== 4) {
         this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
       } else {
         this.wheels = wheels;
-      }
+      }      
+    }  
     }
 
 
