b866777b97463f2de14ac12746e09fa54ec86a35
FORTIS Roberto <roberto.4ti@gmail.com>
2024-11-06 21:02:06 -0500

    components modified

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -402,10 +402,14 @@ class Cli {
         }
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. 
         //Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
-        else if (answers.action === 'Tow' && answers.vehicleType === 'Truck') {
-          //RFS
-          // Truck.tow(vehicle);
-          // this.vehicles.  tow(vehicle);
+        else if (answers.action === 'Tow') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin && this.selectedVehicleVin === 'Truck') {              .
+              //Truck.tow(this.vehicles[i]); RFS
+            }
+          }          
+          // Truck.tow(vehicle); RFS
+          // this.vehicles.tow(vehicle); RFS
         }              
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
         else if (answers.action === 'wheelie' && answers.vehicleType === 'Motorbike') { 
--- src/classes/Vehicle.ts
+++ src/classes/Vehicle.ts
@@ -6,6 +6,7 @@ class Vehicle implements Driveable {
   // Declare properties of the Vehicle class
   started: boolean;
   currentSpeed: number;
+  static make: string;
 
   // Constructor for the Vehicle class
   constructor() {
