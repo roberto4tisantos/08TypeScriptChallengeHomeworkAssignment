512762edde08ba32892ec2554b78eeaa26260878
FORTIS Roberto <roberto.4ti@gmail.com>
2024-11-08 20:40:07 -0500

    Motorbike.ts modified

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -58,7 +58,7 @@ class Motorbike extends Vehicle {
 
   // TODO: Implement the wheelie method
   // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-  wheelie(): void {
+  override wheelie(): void {
     console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
   }
 
